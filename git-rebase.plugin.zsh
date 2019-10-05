1ac5d687109a3de6d50adc3e5c0b9c709be2c0a2
Eleni Lixourioti <contact@eleni.co>
2019-10-05 16:28:03 +0200

    Completions WIP

--- git-rebase.plugin.zsh
+++ git-rebase.plugin.zsh
@@ -1,5 +1,6 @@
 rebase() {
     stashArg='--no-autostash'
+    commit=$1
 
     if ! git diff-index --quiet HEAD; then
         # We only care about stashing if there are unstaged changes
@@ -12,10 +13,19 @@ rebase() {
     fi
 
 
-    git log --color --graph --pretty=format:'%Cgreen%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit | cat -n | less
+    #git log --color --graph --pretty=format:'%Cgreen%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit | cat -n | less
 
-    printf "Commit #: "
-    read -r commit
 
     git rebase -i --autosquash $stashArg HEAD~$commit
+}
+
+compdef _add_completion rebase
+
+
+_add_completion() {
+    local -a commits
+    # TODO: This quoting is dubious
+    commits=`git log --pretty=oneline --abbrev-commit`
+    commits=(${(f)"$(awk '{print NR "\\:\x27"  $s "\x27"}' <<< $commits)"})
+    _arguments "1: :(($^commits))"
 }
